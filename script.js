c3f3a9a19391418e8726b7fe7ea656d62aeb1812
Uchastkavoy <uchastkavoy@kali.kali>
2026-05-12 19:19:09 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+async function loadNews(){
+
+  try{
+
+    const response =
+      await fetch("/news");
+
+    const data =
+      await response.json();
+
+    console.log(data);
+
+    const newsDiv =
+      document.getElementById("news");
+
+    newsDiv.innerHTML = "";
+
+    data.forEach(item => {
+
+      const card =
+        document.createElement("div");
+
+      card.className = "card";
+
+      card.innerHTML = `
+        ${item.image ? `<img src="${item.image}">` : ""}
+        <p>${item.text}</p>
+        <div class="date">
+          ${new Date(item.date).toLocaleString()}
+        </div>
+      `;
+
+      newsDiv.appendChild(card);
+
+    });
+
+  }catch(err){
+
+    console.log(err);
+
+  }
+
+}
+
+loadNews();
+
+setInterval(loadNews,5000);
---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+const fs = require("fs");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(__dirname));
+
+const TOKEN = "8831405808:AAHa_cuh46iIArt6rUXdZyxgyPgnsII3Q2c";
+
+const bot = new TelegramBot(TOKEN, {
+  polling: true
+});
+
+const FILE = "./news.json";
+
+function getNews() {
+
+  if (!fs.existsSync(FILE)) {
+    return [];
+  }
+
+  return JSON.parse(
+    fs.readFileSync(FILE)
+  );
+
+}
+
+function saveNews(news) {
+
+  fs.writeFileSync(
+    FILE,
+    JSON.stringify(news, null, 2)
+  );
+
+}
+
+bot.on("message", async (msg) => {
+
+  let news = getNews();
+
+  let post = {
+    text: msg.text || "",
+    date: new Date()
+  };
+
+  if (msg.photo) {
+
+    const photo =
+      msg.photo[msg.photo.length - 1];
+
+    const file =
+      await bot.getFile(photo.file_id);
+
+    post.image =
+      `https://api.telegram.org/file/bot${TOKEN}/${file.file_path}`;
+
+  }
+
+  news.unshift(post);
+
+  news = news.slice(0,3);
+
+  saveNews(news);
+
+  console.log("NEW POST:", post);
+
+});
+
+app.get("/news", (req,res)=>{
+
+  res.json(getNews());
+
+});
+
+app.get("/", (req,res)=>{
+
+  res.sendFile(
+    path.join(__dirname,"index.html")
+  );
+
+});
+
+app.listen(3000, ()=>{
+
+  console.log("SERVER ISHLADI 3000");
+
+});
